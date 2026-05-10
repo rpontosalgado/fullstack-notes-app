09cedd730ccab6b2a5a3773a5407b628d9166b20
rpontosalgado <r.salgado3@gmail.com>
2026-05-10 17:26:18 -0300

    fix(backend): add controller and service to notes module

--- backend/src/notes/notes.controller.ts
+++ backend/src/notes/notes.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { FilterNotesDto } from './dto/filter-notes.dto';
+import { NotesService } from './notes.service';
 
 @Controller('api/v1/notes')
 export class NotesController {
-  constructor() {}
+  constructor(private readonly notesService: NotesService) {}
 
   @Get()
   getAllNotes(@Query() filters: FilterNotesDto) {
-    return filters;
+    return this.notesService.getAllNotes(filters);
   }
 }
