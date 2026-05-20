9caddfbb6eae2099a46c80883387c7e6caf0f01a
rpontosalgado <r.salgado3@gmail.com>
2026-05-20 02:32:25 -0300

    build(backend): add migration scripts and generate create notes table schema

---
+++ backend/src/database/migrations/1779254690268-CreateNotesTable.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateNotesTable1779254690268 implements MigrationInterface {
+  name = 'CreateNotesTable1779254690268';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "notes" ("id" uuid NOT NULL, "site" character varying NOT NULL, "equipment" character varying NOT NULL, "variable" character varying NOT NULL, "timestamp" TIMESTAMP WITH TIME ZONE NOT NULL, "author" character varying NOT NULL, "message" text NOT NULL, CONSTRAINT "PK_af6206538ea96c4e77e9f400c3d" PRIMARY KEY ("id"))`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "notes"`);
+  }
+}
